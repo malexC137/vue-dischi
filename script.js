8a2c5d7fd1254d34c06159282100e350d1d2d492
malexC137 <71665644+malexC137@users.noreply.github.com>
2021-05-13 01:43:50 +0200

    Project completed

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 new Vue({
     el: "#app",
     data: {
-        albumsList: []
+        albumsList: [],
+        selectedGenre: 'All'
     },
     mounted() {
         const newAlbumsList = [];
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
             .then((resp) => {
                 this.albumsList = resp.data.response
+                this.albumsList.sort((a, b) => parseFloat(a.year) - parseFloat(b.year));
             })
+    },
+    computed: {
+        filteredGenres() {
+            const genresArr = []
+            this.albumsList.forEach((element) => { 
+                if (genresArr.indexOf(element.genre) === -1) {
+                    genresArr.push(element.genre)
+                }
+            });
+            return genresArr;
+        },
+
+        filteredAlbums() {
+            return this.albumsList.filter((element) => {
+
+                if (this.selectedGenre === 'All') {
+                    return true
+                } else {
+                    return element.genre === this.selectedGenre
+                }
+            });
+        },
+    },
+    methods: {
 
     }
 })
